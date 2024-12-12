85fcfdc77426bd0e315194b3503152115b6124f6
UseR <UseR@DESKTOP-C0A9EBU>
2024-12-12 22:52:32 +0200

    Add project files.

---
+++ first c#1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Assignment2
+
+{
+    internal class Program
+    {
+        class Person { public string Name { get; set; } }
+        static void Main(string[] args)
+        {
+            #region Write a program that allows the user to enter a number then print it.
+
+            Console.Write("Please enter a valid number : ");
+            string input = Console.ReadLine();
+
+
+            if (double.TryParse(input, out double number))
+            {
+                Console.WriteLine("You have entered: " + number);
+            }
+            else
+            {
+                Console.WriteLine("The entered value is not a valid number!");
+            }
+
+            #region MyRegion
+            #region Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+
+
+            int number1 = 20;
+            int number2 = number1;
+            Console.WriteLine("Original value of number1: " + number1);
+            Console.WriteLine("Original value of number2: " + number2);
+
+
+            number1 = 30;
+
+
+            Console.WriteLine("New value of number1 after modification: " + number1);
+            Console.WriteLine("Value of number2 remains: " + number2);
+
+            #region Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+
+            Person person1 = new Person { Name = "yosuf" };
+            Person person2 = person1;
+            Console.WriteLine("Before modification:");
+            Console.WriteLine("person1.Name: " + person1.Name);
+            Console.WriteLine("person2.Name: " + person2.Name);
+            person2.Name = "omar";
+            Console.WriteLine("After modification:");
+            Console.WriteLine("person1.Name: " + person1.Name);
+            Console.WriteLine("person2.Name: " + person2.Name);
+            #endregion
+            #endregion
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
+
